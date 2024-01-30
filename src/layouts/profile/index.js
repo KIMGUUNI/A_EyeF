a2e6c96b15ab5e0eb1c9318b073244d63df9675d
jowonaje <jowonjae07@gmail.com>
2024-01-30 14:46:15 +0900

    원제 prettier 수정

--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -1,56 +1,14 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-
-// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
 
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Rayout from "layouts/profile/components/Rayout"
 import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
-import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
-
-// Overview page components
 import Header from "layouts/profile/components/Header";
-import PlatformSettings from "layouts/profile/components/PlatformSettings";
 
-// Data
-import profilesListData from "layouts/profile/data/profilesListData";
-
-// Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import homeDecor4 from "assets/images/home-decor-4.jpeg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
-import MDInput from "components/MDInput";
 
 function Overview() {
   return (
@@ -70,11 +28,139 @@ function Overview() {
             <Grid item xs={12} xl={4}></Grid>
           </Grid>
           {/* 여기다 글 작성 가능 */}
-          <MDInput></MDInput>
+          dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dassdddddddddd
+        dsaadsasddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dassddddddddddddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dassddddddddddddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dassdddddddddd
         </MDBox>
       </Header>
-      <Header>
-      </Header>
+      <Rayout>
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasaddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+        dassdddddddddd
+        das
+        dashboardasddsasad
+        dashboardasddsasaddasasd
+        dashboardasddsasaddasasddsaasdasdsd
+        dassddddddddddsd
+        dashboardasddsasaddasasddsaasdasdsd
+        dsaadsas
+      </Rayout>
       <Footer />
     </DashboardLayout>
   );
