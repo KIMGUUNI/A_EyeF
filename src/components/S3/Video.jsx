abd1ddf29b3b9efc6537663e6a1dceeb4818a247
김찬혁 <0624564326aa@gmail.com>
2024-02-16 17:10:55 +0900

    동영상 재생 완료시 재생횟수 +1 추가 푸시

--- src/components/S3/Video.jsx
+++ src/components/S3/Video.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import axios from 'axios';
 
 const Video = () => {
-  const [videoUrls, setVideoUrls] = useState({current: [], next: []});
+  const [videoUrls, setVideoUrls] = useState({ current: [], next: [] });
 
   useEffect(() => {
     const socket = new WebSocket("wss://lrdvilsfcd.execute-api.ap-northeast-2.amazonaws.com/production/");
 
     socket.addEventListener('open', (event) => {
-        console.log('WebSocket 연결이 열렸습니다.', event);
-      });
+      console.log('WebSocket 연결이 열렸습니다.', event);
+    });
 
     socket.addEventListener("message", (event) => {
       const data = JSON.parse(event.data);
       setVideoUrls(prevUrls => {
         if (prevUrls.current.length === 0) {
-          return {current: data.video_urls, next: prevUrls.next};
+          return { current: data.video_urls, next: prevUrls.next };
         } else {
           const nextFirstUrl = prevUrls.next.length > 0 ? [prevUrls.next[0]] : [];
-          return {current: prevUrls.current, next: [...nextFirstUrl, ...data.video_urls]};
+          return { current: prevUrls.current, next: [...nextFirstUrl, ...data.video_urls] };
         }
       });
     });
@@ -29,23 +30,39 @@ const Video = () => {
   const playNextVideo = () => {
     setVideoUrls(prevUrls => {
       if (prevUrls.next.length > 0) {
-        return {current: prevUrls.next, next: []};
+        sendUrlToSpringBoot(prevUrls.current[0]);
+        return { current: prevUrls.next, next: [] };
       } else {
-        return {current: prevUrls.current.slice(1), next: prevUrls.next};
+        sendUrlToSpringBoot(prevUrls.current[0]);
+        return { current: prevUrls.current.slice(1), next: prevUrls.next };
       }
     });
   };
-  console.log(videoUrls)
+
+  const sendUrlToSpringBoot = async (url) => {
+    try {
+      const axiosInstance = axios.create({
+        baseURL: "http://localhost:8089/A_Eye",
+        withCredentials: true,
+      });
+      const response = await axiosInstance.post('/api/s3Url', { url });
+      
+      console.log('URL이 성공적으로 전송되었습니다.',response.data);
+    } catch (error) {
+      console.error('HTTP POST 요청 중 오류 발생:', error);
+    }
+  };
+
   return (
     <div>
       {videoUrls.current.length > 0 ? (
-        <ReactPlayer 
-          url={videoUrls.current[0]} 
-          controls 
-          width="100%" 
-          height="100%" 
-          playing 
-          onEnded={playNextVideo} 
+        <ReactPlayer
+          url={videoUrls.current[0]}
+          controls
+          width="100%"
+          height="100%"
+          playing
+          onEnded={playNextVideo}
         />
       ) : (
         <p>동영상 로딩 중...</p>
