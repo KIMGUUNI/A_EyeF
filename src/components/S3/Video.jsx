f0cb5be57837e7db56dc4b3fed8be9dfed399883
김찬혁 <0624564326aa@gmail.com>
2024-02-19 17:32:18 +0900

    중복재생 커밋

--- src/components/S3/Video.jsx
+++ src/components/S3/Video.jsx
@@ -4,6 +4,11 @@ import axios from 'axios';
 
 const Video = () => {
   const [videoUrls, setVideoUrls] = useState({ current: [], next: [] });
+  const [key, setKey] = useState(0);
+
+  const handleVideoEnded = () => {
+    setKey(prevKey => prevKey + 1);
+  };
 
   useEffect(() => {
     const socket = new WebSocket("wss://lrdvilsfcd.execute-api.ap-northeast-2.amazonaws.com/production/");
@@ -14,14 +19,23 @@ const Video = () => {
 
     socket.addEventListener("message", (event) => {
       const data = JSON.parse(event.data);
+      const playedVideos = JSON.parse(sessionStorage.getItem("playedVideos")) || [];
+
       setVideoUrls(prevUrls => {
+        const newVideoUrls = data.video_urls.filter(url => 
+          !prevUrls.current.includes(url) && 
+          !prevUrls.next.includes(url) &&
+          !playedVideos.includes(url)
+        );
+
         if (prevUrls.current.length === 0) {
-          return { current: data.video_urls, next: prevUrls.next };
+          return { current: newVideoUrls, next: prevUrls.next };
         } else {
           const nextFirstUrl = prevUrls.next.length > 0 ? [prevUrls.next[0]] : [];
-          return { current: prevUrls.current, next: [...nextFirstUrl, ...data.video_urls] };
+          return { current: prevUrls.current, next: [...nextFirstUrl, ...newVideoUrls] };
         }
       });
+      console.log("재생한 동영상",videoUrls)
     });
 
     return () => socket.close();
@@ -29,8 +43,9 @@ const Video = () => {
 
   const playNextVideo = () => {
     setVideoUrls(prevUrls => {
-      if (prevUrls.next.length > 0) {
+      if (prevUrls.current.length > 0) {
         sendUrlToSpringBoot(prevUrls.current[0]);
+        sessionStorage.setItem("playedVideos", JSON.stringify([prevUrls.current[0], ...JSON.parse(sessionStorage.getItem("playedVideos")) || []]));
         return { current: prevUrls.next, next: [] };
       } else {
         sendUrlToSpringBoot(prevUrls.current[0]);
@@ -53,6 +68,12 @@ const Video = () => {
     }
   };
 
+  useEffect(() => {
+    if (videoUrls.current.length === 0 && videoUrls.next.length === 0) {
+      sessionStorage.removeItem("playedVideos");
+    }
+  }, [videoUrls]);
+
   return (
     <div>
       {videoUrls.current.length > 0 ? (
@@ -65,7 +86,15 @@ const Video = () => {
           onEnded={playNextVideo}
         />
       ) : (
-        <p>동영상 로딩 중...</p>
+        <ReactPlayer 
+        key={key}
+        url="https://video-add.s3.ap-northeast-2.amazonaws.com/default/%5B%EA%B3%B5%EC%9D%B5%EA%B4%91%EA%B3%A0%ED%98%91%EC%9D%98%ED%9A%8C%5D+%ED%94%BC%EC%8B%B1%C2%B7%EC%8A%A4%EB%AF%B8%EC%8B%B1+%EC%98%88%EB%B0%A9+-+%EC%8B%AC%EC%B2%AD(viral).mp4"
+        controls
+          width="100%"
+          height="100%"
+          playing
+          onEnded={handleVideoEnded}/>
+
       )}
     </div>
   );
